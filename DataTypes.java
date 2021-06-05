0b44dee711339c9de44a2c6e47afedc55a8c9e7a
unknown <agusmaulana120@gmail.com>
2021-06-05 22:07:35 +0700

    Deklarasi variabel dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+ public static void main(String[] args){
+
+  String namaDepan = "Agus";
+  String namaTengah = "Mau";
+  String namaBelakang= "lana";
+  int usia = 18;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+ char nilaiAbjad = 'A';
+ boolean tampan = true;
+
+  }
+}
