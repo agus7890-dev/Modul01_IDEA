4c578280d189a73f29af69b8b0b5c1d1a8f8bf6a
unknown <agusmaulana120@gmail.com>
2021-06-05 22:39:34 +0700

    Output dengan sout

--- DataTypes.java
+++ DataTypes.java
@@ -4,11 +4,22 @@ public class DataTypes{
   String namaDepan = "Agus";
   String namaTengah = "Mau";
   String namaBelakang= "lana";
-  int usia = 18;
-  int targetTahunKuliah = 4;
-  double ipk = 3.89764512;
- char nilaiAbjad = 'A';
- boolean tampan = true;
+  int usia = 19;
+  int lamaKuliah = 4;
+  double ipk;
+  char nilai = 'A';
+  boolean tampan = true;
+
+  System.out.print("Nama depan : ");
+  System.out.println(namaDepan);
+  System.out.println("Nama tengah : " +namaTengah);
+  System.out.println("Nama belakang : " +namaBelakang );
+  System.out.println("Usia : " +usia);
+  System.out.println("Usia saat lulus (String) : " + usia + lamaKuliah );
+  System.out.println("Usia saat lulus (kurung) : " + (usia + lamaKuliah ));
+
+  int UsiaLulus = usia + lamaKuliah;
+  System.out.println("Usia saat lulus (variable) : " + (usia + lamaKuliah ));
 
   }
 }
